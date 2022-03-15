a500a482965402db1bd1da47dad17f35878d9bb3
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-03-15 14:39:33 +0100

    Created Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,7 +5,7 @@ function Sidebar() {
   return (
     <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[320px] p-2 fixed h-full">
         <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
-          <Image src="https://rb.gy/ogau5a"/>
+          <Image src="https://rb.gy/ogau5a" width={}/>
         </div>
     </div>
   )
