1b1f725ab3c9540e0bff4cfa1c61f5b1fd82dfa2
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-03-15 14:38:33 +0100

    Created Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 function Sidebar() {
   return (
     <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[320px] p-2 fixed h-full">
-        <div className="flex items-center justify-center w-14 h-14 hoverAnimation">
-          <Image/>
+        <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
+          <Image src="https://rb.gy/ogau5a"/>
         </div>
     </div>
   )
