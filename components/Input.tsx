cb221a9d672e798b3e97441c5effe53452e21f8c
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-03-18 09:45:27 +0100

    Modified Input Section

--- components/Input.tsx
+++ components/Input.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 function Input() {
   return (
-    <div>
-      
+    <div className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll`}>
+      <img src="https://i.pcmag.com/imagery/reviews/03aizylUVApdyLAIku1AvRV-39.fit_scale.size_1028x578.v1605559903.png" alt="" className="h-11 w-11 rounded-full cursor-pointer"/>
+      <div className="w-full divide-y divide-gray-700">
+        <div>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+        </div>
+        <div>
+          Hello
+        </div>
+      </div>
     </div>
   )
 }
