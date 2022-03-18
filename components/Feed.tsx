b8cadaf79c38fa45df1fa6f55cc7cca40ec646a5
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-03-18 09:18:03 +0100

    Started Feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Feed() {
   return (
-    <div className="text-white">
-      Feed
+    <div className="text-white flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
+      <div>
+        
+      </div>
     </div>
   )
 }
