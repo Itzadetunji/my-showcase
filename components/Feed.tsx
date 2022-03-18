d8a141cdeca2a0be15a8a9a4496946606c2b190a
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-03-18 09:19:29 +0100

    Improved Feed Section

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Feed() {
   return (
     <div className="text-white flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
-      <div>
+      <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky">
         
       </div>
     </div>
