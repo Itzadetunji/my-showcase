db977f637cca0513f3454f6bbb390b5074a76c50
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-Air.mshome.net>
2022-03-22 19:36:50 +0100

    Updated Input section

--- components/Input.tsx
+++ components/Input.tsx
@@ -8,6 +8,11 @@ function Input() {
   const [selectedFile, setSelectedFile] = useState(null)
   const [showEmojis, setShowEmojis] = useState(false)
   const filePickerRef = useRef(null)
+
+  const sendPost = () => {
+    
+  }
+
   const addImageToPost = () => {};
 
   const addEmoji = (e) => {
@@ -16,14 +21,14 @@ function Input() {
     sym.forEach((el) => codesArray.push("0x" + el));
     let emoji = String.fromCodePoint(...codesArray);
     setInput(input + emoji);
-  }
+  };
 
   const [addImageToPos, setAddImageToPos] = useState(null)
   return (
     <div className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll`}>
       <img src="https://i.pcmag.com/imagery/reviews/03aizylUVApdyLAIku1AvRV-39.fit_scale.size_1028x578.v1605559903.png" alt="" className="h-11 w-11 rounded-full cursor-pointer"/>
       <div className="w-full divide-y divide-gray-700">
-        <div className={`${selectedFile && "pb-7"}` ${input && "space-y-2.5"}}>
+        <div className={`${selectedFile && "pb-7"} ${input && "space-y-2.5"}`}>
           <textarea 
           value={input}
           onChange={(e) => setInput(e.target.value)} 
