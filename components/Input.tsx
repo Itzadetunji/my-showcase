2db80d8807cafac04348d7f3b17882f5d4666309
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-Air.mshome.net>
2022-03-22 19:11:29 +0100

    Added Emoji-Mart to Input section

--- components/Input.tsx
+++ components/Input.tsx
@@ -9,6 +9,7 @@ function Input() {
   const [showEmojis, setShowEmojis] = useState(false)
   const filePickerRef = useRef(null)
   const addImageToPost = () => {};
+  const [addImageToPos, setAddImageToPos] = useState(null)
   return (
     <div className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll`}>
       <img src="https://i.pcmag.com/imagery/reviews/03aizylUVApdyLAIku1AvRV-39.fit_scale.size_1028x578.v1605559903.png" alt="" className="h-11 w-11 rounded-full cursor-pointer"/>
@@ -59,10 +60,11 @@ function Input() {
                   style={{
                     position: "absolute",
                     marginTop: "465px",
-                    marginLeft: "-40",
+                    marginLeft: -40,
                     maxWidth: "320px",
                     borderRadius: "20px"
                   }}
+                  theme="dark"
                 />
               )}
             </div>
