8b081e5d571c9322a6f435cfe1d54959a83e9af7
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-Air.mshome.net>
2022-03-25 21:00:36 +0100

    Creating Nesxt-js authentication

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,3 +23,23 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export async function getServerSideProps() {
+  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+    (res) => res.json()
+  );
+  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+    (res) => res.json()
+  );
+  const providers = await getProviders();
+  const session = await getSession(context);
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  };
+}
