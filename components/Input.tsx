5ea8beafb1aa2fd89d858c45424e58aeba1b2072
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-Air.mshome.net>
2022-03-25 18:25:05 +0100

    Started firebase connection

--- components/Input.tsx
+++ components/Input.tsx
@@ -7,10 +7,11 @@ import {
   addDoc,
   collection,
   doc,
-  serverTimeStamp,
+  serverTimestamp,
   updateDoc,
 } from "@firebase/firestore";
 import { getDownloadURL, ref, uploadString } from "@firebase/storage"
+import { Session } from 'inspector';
 
 function Input() {
   const [input, setInput] = useState("")
@@ -23,7 +24,14 @@ function Input() {
     if(loading) return;
     setLoading(true);
 
-    const docRef = await addDoc()
+    const docRef = await addDoc(collection(db, 'posts'), {
+      // id: session.user.uid,
+      // username: session.user.name,
+      // userImg: session.user.image,
+      // tag: session.user.tag,
+      text: input,
+      timestamp: serverTimestamp()
+    })
   }
 
   const addImageToPost = () => {};
