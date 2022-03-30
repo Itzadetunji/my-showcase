9c220e1f0a2b65e0b55cffa8b17a3d6e54d98500
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-03-30 13:47:13 +0100

    Started login page with next authentication

--- components/login.tsx
+++ components/login.tsx
@@ -1,8 +1,17 @@
+import { signIn } from "next-auth/react";
+import Image from "next/image";
 import React from 'react'
 
 function login() {
   return (
-    <div>login</div>
+    <div className="text-white">
+      <Image
+        src="https://rb.gy/ogau5a"
+        width={150}
+        height={150}
+        objectFit="contain"
+      />
+    </div>
   )
 }
 
