72c57b66ebd0fec9e952c7d5c0198ffd21e4c47d
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-Air.mshome.net>
2022-03-31 20:22:53 +0100

    Displaying posts using the post component

--- components/Post.tsx
+++ components/Post.tsx
@@ -4,8 +4,13 @@ function Post({ id, post, postPage }) {
   return (
     <div className="p-3 flex cursor-pointer border-b border-gray-700">
       {!postPage && (
-        <img src={post?.userImg} alt="" />
+        <img src={post?.userImg} alt=""  className="h-11 w-11 rounded-full mr-4"/>
       )}
+      <div className="flex flex-col space-y-2 w-full">
+        <div className={`flex $(!postPage && "justify-between")`}>
+          
+        </div>
+      </div>
     </div>
   )
 } 
