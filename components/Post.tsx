e115ce01f712232d3498ca3a1ab487e33d13c438
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-Air.mshome.net>
2022-03-31 20:20:36 +0100

    Displaying posts using the post component

--- components/Post.tsx
+++ components/Post.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Post({ id, post, postPage }) {
   return (
     <div className="p-3 flex cursor-pointer border-b border-gray-700">
-      
+      {!postPage && (
+        <img src={post?.userImg} alt="" />
+      )}
     </div>
   )
 } 
