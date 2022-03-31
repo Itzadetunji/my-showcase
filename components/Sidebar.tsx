1b1efa01f19fafa0f3e9615a5bd504f7772d25f1
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-Air.mshome.net>
2022-03-31 19:03:34 +0100

    Started displaying data with next auth

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,8 +12,10 @@ import {
   UserIcon,
   DotsCircleHorizontalIcon,
 } from "@heroicons/react/outline";
+import { useSession } from 'next-auth/react';
 
 function Sidebar() {
+  const { data : session } = useSession();
   return (
     <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[320px] p-2 fixed h-full">
         <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
@@ -31,9 +33,9 @@ function Sidebar() {
         </div>
         <button className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]">Tweet</button>
         <div className="text-[#d9d9d9] flex items-center justify-center hoverAnimation xl:ml-auto xl:-mr-5 mt-auto">
-          <img src="https://i.pcmag.com/imagery/reviews/03aizylUVApdyLAIku1AvRV-39.fit_scale.size_1028x578.v1605559903.png" alt="" className="h-10 w-10 rounded-full xl:mr-2.5"/>
+          <img src={session.user.image} alt="" className="h-10 w-10 rounded-full xl:mr-2.5"/>
           <div className="hidden xl:inline leading-5">
-            <h4 className="font-bold">Adetunji Adyinka</h4>
+            <h4 className="font-bold">{session.user.name}</h4>
             <p className="text-[#6e767d]">@itzadetunji</p>
           </div>
           <DotsCircleHorizontalIcon className="h-5 hidden xl:inline ml-10"/>
