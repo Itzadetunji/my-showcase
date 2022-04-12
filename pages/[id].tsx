60dc898f397354143a591c11c6698475244bb1a1
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-Air.mshome.net>
2022-04-12 12:46:07 +0100

    Started single post page

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,18 +1,38 @@
-import { useSession } from 'next-auth/react';
-import Head from 'next/head'
-import { useRouter } from 'next/router';
-import React from 'react'
-import { useRecoilState } from 'recoil';
-import { modalState } from '../atoms/modalAtom';
-import Modal from '../components/Modal'
-import Sidebar from '../components/Sidebar'
 
+import {
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "@firebase/firestore";
+import { getProviders, getSession, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { modalState } from "../atoms/modalAtom";
+import Modal from "../components/Modal";
+import Sidebar from "../components/Sidebar";
+// import Widgets from "../components/Widgets";
+import Post from "../components/Post";
+import { db } from "../firebase";
+import { ArrowLeftIcon } from "@heroicons/react/solid";
+// import Comment from "../components/Comment";
+import Head from "next/head";
 function PostPage() {
   const { data : session } = useSession();
   const [isOpen, setIsOpen] = useRecoilState(modalState);
   const router = useRouter();
   const { id } = router. query; // or const id = router.query.id
   if(!session){return false}
+  
+  useEffect(
+    () =>
+      onSnapshot(doc(db, "posts", id), (snapshot) => {
+        setPost(snapshot.data());
+      }),
+    [db]
+  );
   return (
     <div className="">
       <Head>
