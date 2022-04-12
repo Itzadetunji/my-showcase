c2498fae21bef22807cd1afd7101714bf3faa047
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-04-12 11:43:45 +0100

    Started single post page

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,8 +1,22 @@
+import { Head } from 'next/document'
 import React from 'react'
+import Modal from '../components/Modal'
+import Sidebar from '../components/Sidebar'
 
 function PostPage() {
   return (
-    <div>[id]</div>
+    <div className="">
+      <Head>
+        <title>Twitter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head> 
+      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
+        <Sidebar/>
+        {/* Widgets */}
+
+        { isOpen && <Modal/> }
+      </main>
+    </div>
   )
 }
 
