ae0b91d68299bce14afef15e3c4e1035e14183fa
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-Air.mshome.net>
2022-04-12 12:44:13 +0100

    Started single post page

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,9 +1,19 @@
-import { Head } from 'next/document'
+import { useSession } from 'next-auth/react';
+import Head from 'next/head'
+import { useRouter } from 'next/router';
 import React from 'react'
+import { useRecoilState } from 'recoil';
+import { modalState } from '../atoms/modalAtom';
 import Modal from '../components/Modal'
 import Sidebar from '../components/Sidebar'
 
 function PostPage() {
+  const { data : session } = useSession();
+  const [isOpen, setIsOpen] = useRecoilState(modalState);
+  const router = useRouter();
+  // const { id } = router. query;
+  console.log(router)
+  if(!session){return false}
   return (
     <div className="">
       <Head>
@@ -17,7 +27,7 @@ function PostPage() {
         { isOpen && <Modal/> }
       </main>
     </div>
-  )
+  );
 }
 
 export default PostPage
