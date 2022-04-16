2d6dc47844c3b213ca0fd3ccc9d9aa8dbae15044
Adetunji Peter Adeyinka <itzadetunji@Adetunjis-MacBook-Air.local>
2022-04-16 21:31:32 +0100

    Working on Widgets Page

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 
 function Widgets() {
   return (
-    <div className="hidden lg:inline ml-0 xl:w-[450px] py-1 space-y-5">
+    <div className="hidden lg:inline ml-8 xl:w-[450px] py-1 space-y-5">
       <div className="sticky top-0 py-1.5 bg-black z-50 w-11/12 xl:w-9/12">
-        <SearchIcon className="text-gray-500 h-5 z-50"/>
+        <div className="flex items-center bg-[#202327] p-3 rounded-full relative">
+          <SearchIcon className="text-gray-500 h-5 z-50"/>
+          <input type="text" className="bg-transparent placeholder-gray-500 outline-none text-[#d9d9d9] inset-0 pl-11 border border-transparent w-full" placeholder="Search Twitter"/>
+        </div>
       </div>
     </div>
   )
